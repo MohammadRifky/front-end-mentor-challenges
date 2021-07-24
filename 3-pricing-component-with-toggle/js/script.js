413a7b12308510f0d6ac56d7c1e477e0c821a4a7
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2021-07-24 19:46:00 +0530

    optimized for production

--- 3-pricing-component-with-toggle/js/script.js
+++ 3-pricing-component-with-toggle/js/script.js
@@ -1,14 +1,1 @@
-const monthlyPricing = document.querySelectorAll('.monthly')
-console.log(monthlyPricing)
-const annualPricing = document.querySelectorAll('.annual')
-const checkBox = document.getElementById('price-toggle')
-
-checkBox.addEventListener('click', () => {
-    annualPricing.forEach( annualItem => {
-        annualItem.classList.toggle('hidden')
-    })
-    monthlyPricing.forEach( monthlyItem => {
-        console.log(monthlyItem)
-        monthlyItem.classList.toggle('hidden')
-    })
-})
+const monthlyPricing=document.querySelectorAll(".monthly"),annualPricing=document.querySelectorAll(".annual"),checkBox=document.getElementById("price-toggle");checkBox.addEventListener("click",(()=>{annualPricing.forEach((e=>{e.classList.toggle("hidden")})),monthlyPricing.forEach((e=>{e.classList.toggle("hidden")}))}));
