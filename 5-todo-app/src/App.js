c71bb8f59a07efae37621e2d4a19dd6f86a5870d
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2021-11-24 20:17:16 +0530

    completed configuration

--- 5-todo-app/src/App.js
+++ 5-todo-app/src/App.js
@@ -2,7 +2,11 @@ import ToggleThemeButton from "./components/ToggleThemeButton";
 import ToggleCompleteButton from "./components/ToggleCompleteButton";
 const App = () => {
   return (
-    <h1 className="text-xl text-red-300">Todo</h1>
+    <>
+      <h1 className="text-xl text-red-700 text-center">Todo</h1>
+      <ToggleThemeButton />
+      <ToggleCompleteButton />
+    </>
   );
 }
 
--- 5-todo-app/tailwind.config.js
+++ 5-todo-app/tailwind.config.js
@@ -1,9 +1,35 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  mode: 'jit',
   darkMode: 'media',
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        'bright-blue': 'hsl(220, 98%, 61%)',
+      },
+      light : {
+        'vl-gray': 'hsl(0, 0%, 98%)',
+        'vl-grayish-blue': 'hsl(236, 33%, 92%)',
+        'l-grayish-blue': 'hsl(233, 11%, 84%)',
+        'd-grayish-blue': 'hsl(236, 9%, 61%)',
+        'vd-grayish-blue': 'hsl(235, 19%, 35%)'
+      },
+      dark : {
+        'vd-blue': 'hsl(235, 21%, 11%)',
+        'vd-des-blue': 'hsl(235, 24%, 19%)',
+        'l-grayish-blue': 'hsl(234, 39%, 85%)',
+        'l-grayish-blue-hover': 'hsl(236, 33%, 92%)',
+        'd-grayish-blue': 'hsl(234, 11%, 52%)',
+        'vd-grayish-blue': 'hsl(233, 14%, 35%)',
+        'vd-grayish-blue': 'hsl(237, 14%, 26%)'
+      }
+    },
+    fontFamily: {
+      'main': ['Josefin Sans', 'sans-serif']
+    },
+    fontWeight : {
+      normal: 400,
+      bold: 700,
+    }
   },
   variants: {
     extend: {},
