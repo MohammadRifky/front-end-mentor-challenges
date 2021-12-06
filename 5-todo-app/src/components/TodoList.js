39a574b3ea115f467142254115d8f5f654487887
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2021-12-06 18:35:23 +0530

    worked on drag and drop feature

--- 5-todo-app/src/components/TodoList.js
+++ 5-todo-app/src/components/TodoList.js
@@ -1,28 +1,48 @@
 import React, { useState } from 'react'
 import Todo from './Todo'
 import { useSelector } from 'react-redux'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 const TodoList = () => {
     const todos = useSelector((state) => state.todos.todos)
     const [todosToDisplay, setTodosToDisplay] = useState(todos)
-    const getActiveTodos = () => todos.filter(todo => 
+    const getActiveTodos = () => todos.filter(todo =>
         todo.isCompleted !== true)
-    const getCompletedTodos = () => todos.filter(todo => 
+    const getCompletedTodos = () => todos.filter(todo =>
         todo.isCompleted !== false)
     // const uncompletedTodos = getActiveTodos().length
     return (
         <div>
-            { todosToDisplay.map((todo, index) => (
-                <Todo key={todo.id} todoIndex={index}  />
+            <DragDropContext>
+                <Droppable droppableId="todos">
+                    {(provided) => (
+                        <ul className="todos" {...provided.droppableProps} ref={provided.innerRef}>
+                            {finalSpaceCharacters.map(({ id, name, thumb }) => {
+                                return (
+                                    <Draggable>
+                                        {(provided) => (
+                                            <li key={id}>
+                                                ...
+                                            </li>
+                                        )}
+                                    </Draggable>
+                                )
+                            })}
+                        </ul>
+                    )}
+                </Droppable>
+            </DragDropContext>
+            {todosToDisplay.map((todo, index) => (
+                <Todo key={todo.id} todoIndex={index} />
             ))}
             <div>
                 <span>{getActiveTodos().length} items left</span>
-                <button onClick={()=> setTodosToDisplay(todos) }>
+                <button onClick={() => setTodosToDisplay(todos)}>
                     All
                 </button>
-                <button onClick={()=> setTodosToDisplay(getActiveTodos()) }>
+                <button onClick={() => setTodosToDisplay(getActiveTodos())}>
                     Active
                 </button>
-                <button onClick={()=> setTodosToDisplay(getCompletedTodos)}>
+                <button onClick={() => setTodosToDisplay(getCompletedTodos)}>
                     Completed
                 </button>
             </div>
