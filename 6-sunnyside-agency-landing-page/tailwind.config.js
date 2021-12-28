460516f60e508d09b9a001ad1a3574aa20c53318
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2021-12-28 08:20:18 +0530

    initiated colors

--- 6-sunnyside-agency-landing-page/tailwind.config.js
+++ 6-sunnyside-agency-landing-page/tailwind.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      primary : {
+        'red': 'hsl(7, 99%, 70%)',
+        'yellow': 'hsl(51, 100%, 49%)',
+        'desaturated-cyan' : 'hsl(167, 40%, 24%)',
+        'blue' : 'hsl(198, 62%, 26%)',
+        'moderate-cyan' : 'hsl(168, 34%, 41%)'
+      },
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#evebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
     extend: {},
   },
   plugins: [],
