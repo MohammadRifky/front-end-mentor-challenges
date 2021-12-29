8c618921f27d94f1e29bf3b13b65b7a373ce594e
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2021-12-29 16:19:07 +0530

    completed basic layout

--- 6-sunnyside-agency-landing-page/tailwind.config.js
+++ 6-sunnyside-agency-landing-page/tailwind.config.js
@@ -26,6 +26,20 @@ module.exports = {
       backgroundImage: {
         'hero-image': "url('../images/desktop/image-header.jpg')",
         'hero-image-mobile': "url('../images/mobile/image-header.jpg')",
+        'transform-image': "url('../images/desktop/image-transform.jpg')",
+        'transform-image-mobile': "url('../images/mobile/image-transform.jpg')",
+        'stand-out-image': "url('../images/desktop/image-stand-out.jpg')",
+        'stand-out-image-mobile': "url('../images/mobile/image-stand-out.jpg')",
+        'graphic-design-image': "url('../images/desktop/image-graphic-design.jpg')",
+        'graphic-design-image-mobile': "url('../images/mobile/image-graphic-design.jpg')",
+        'photography-image': "url('../images/desktop/image-photography.jpg')",
+        'photography-image-mobile': "url('../images/mobile/image-photography.jpg')",
+        'gallery-orange-image': "url('../images/desktop/image-gallery-orange.jpg')",
+        'gallery-orange-image-mobile': "url('../images/mobile/image-gallery-orange.jpg')",
+        'gallery-cone-image': "url('../images/desktop/image-gallery-cone.jpg')",
+        'gallery-cone-image-mobile': "url('../images/mobile/image-gallery-cone.jpg')",
+        'gallery-milk-bottles-image': "url('../images/desktop/image-gallery-milkbottles.jpg')",
+        'gallery-milk-bottles-image-mobile': "url('../images/mobile/image-gallery-milkbottles.jpg')",
       }
     }
   },
