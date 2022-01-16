f6d26519cf3764df34a35f7fa16591a88193130d
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2022-01-16 23:40:05 +0530

    added down arrow

---
+++ 6-sunnyside-agency-landing-page/js/script.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementById('navbar')
+const toggleNavbar = () => {
+    navbar.classList.toggle('hidden')
+}
+window.onclick = (event) => {
+    if (!event.target.matches('#toggleBtn') && !navbar.classList.contains('hidden')) {
+        toggleNavbar()
+    }
+}
