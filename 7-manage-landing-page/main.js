ab5801361984d81d84847ee28e33e17e82f19525
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2022-01-22 00:23:53 +0530

    added carousel

--- 7-manage-landing-page/main.js
+++ 7-manage-landing-page/main.js
@@ -1,1 +1,17 @@
 console.log("Hello World")
+var slider = tns({
+    container: '.my-slider',
+    slideBy: 'page',
+    autoplay: false,
+    autoplayButtonOutput : false,
+    controls : false,
+    nav	: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768 : {
+        items : 3
+      }
+    }
+});
