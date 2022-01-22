b2126d1aa97812e8d673bd28a8b0ed9a2c463379
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2022-01-22 23:01:11 +0530

    worked on carousel

--- 7-manage-landing-page/main.js
+++ 7-manage-landing-page/main.js
@@ -1,17 +1,20 @@
 var slider = tns({
     container: '.my-slider',
     slideBy: 'page',
-    autoplay: true,
+    //autoplay: true,
     autoplayButtonOutput : false,
     controls : false,
+    nav	: false,
     responsive: {
       0: {
         items: 1,
-        nav : true
+        nav : true,
+        // navContainer : '#indicators',
+        navPosition : 'bottom'
       },
       768 : {
         items : 3,
-        nav	: false
       }
     }
 });
+console.log(document.querySelector('#indicators'))
